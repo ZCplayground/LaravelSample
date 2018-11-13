b29826f67ed3ae96c163ff2bfb026381d5bfd99f
vagrant <vagrant@homestead>
2018-11-13 15:00:10 +0000

    Move user model to models folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,24 +8,24 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * 当我们运行迁移时，up 方法会被调用；
      * @return void
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->increments('id'); // increments 方法创建了一个 integer 类型的自增长 id。
+            $table->string('name'); // 由 string 方法创建了一个 name 字段，用于保存用户名称。
+            $table->string('email')->unique(); // 由 string 方法创建了一个 email 字段，且在最后指定该字段的值为唯一值，用于保存用户邮箱。
+            $table->string('password', 60); // 由 string 方法创建了一个 password 字段，且在 string 方法中指定保存的值最大长度为 60，用于保存用户密码。
+            $table->rememberToken(); // rememberToken 方法为用户创建一个 remember_token 字段，用于保存『记住我』的相关信息。
+            $table->timestamps(); // 由 timestamps 方法创建了一个 created_at 和一个 updated_at 字段，分别用于保存用户的创建时间和更新时间。
         });
     }
 
     /**
      * Reverse the migrations.
-     *
+     * 当我们回滚迁移时，down 方法会被调用。
      * @return void
      */
     public function down()
